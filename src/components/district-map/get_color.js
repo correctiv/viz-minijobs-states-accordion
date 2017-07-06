e2ed92f4786fe39323d39483b5a842ae73044570
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-07-06 12:40:20 +0200

    Update colors: use greys scheme

--- src/components/district-map/get_color.js
+++ src/components/district-map/get_color.js
@@ -1,4 +1,5 @@
 import * as d3 from '../../scripts/lib/d3.js'
 
-const colors = ['#ffffcc', '#ffeda0', '#fed976', '#feb24c', '#fd8d3c', '#fc4e2a', '#e31a1c', '#bd0026', '#800026']
+// const colors = ['#ffffcc', '#ffeda0', '#fed976', '#feb24c', '#fd8d3c', '#fc4e2a', '#e31a1c', '#bd0026', '#800026']
+const colors = ["#ffffff", "#f0f0f0", "#d9d9d9", "#bdbdbd", "#969696", "#737373", "#525252", "#252525", "#000000"]
 export default d3.scaleQuantile().domain([0, 12]).range(colors)
